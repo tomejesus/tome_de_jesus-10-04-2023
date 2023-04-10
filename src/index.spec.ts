8034f28591d45626b5cd6deb18ece33c7275e7b8
Tomé Jesus <46714883+tomejesus@users.noreply.github.com>
2023-04-10 18:20:03 +0100

    Linting

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,134 +1,137 @@
-import { WorldGrid, Robot, MarsMissionSimulator } from './index'
+import { WorldGrid, Robot, MarsMissionSimulator } from './index';
 
 describe('WorldGrid', () => {
   test('WorldGrid is modelled as a grid with size m x n.', () => {
-    const worldGrid = new WorldGrid(5, 3)
-    expect(worldGrid).toBeInstanceOf(WorldGrid)
-    expect(worldGrid.m).toBe(5)
-    expect(worldGrid.n).toBe(3)
-  })
-})
+    const worldGrid = new WorldGrid(5, 3);
+    expect(worldGrid).toBeInstanceOf(WorldGrid);
+    expect(worldGrid.m).toBe(5);
+    expect(worldGrid.n).toBe(3);
+  });
+});
 
 describe('Robot', () => {
-  const worldGrid = new WorldGrid(5, 3)
-  const commands = 'FRFRF'
+  const worldGrid = new WorldGrid(5, 3);
+  const commands = 'FRFRF';
 
   test('Robot has a position (x, y), and an orientation (N, E, S, W).', () => {
-    const robot = new Robot(1, 1, 'E', worldGrid, commands)
-    expect(robot).toBeInstanceOf(Robot)
-    expect(robot.x).toBe(1)
-    expect(robot.y).toBe(1)
-    expect(robot.orientation).toBe('E')
-  })
+    const robot = new Robot(1, 1, 'E', worldGrid, commands);
+    expect(robot).toBeInstanceOf(Robot);
+    expect(robot.x).toBe(1);
+    expect(robot.y).toBe(1);
+    expect(robot.orientation).toBe('E');
+  });
 
   test('Robot can move forward one space (F).', () => {
-    const robot = new Robot(1, 1, 'E', worldGrid, commands)
-    robot.moveForward()
-    expect(robot.x).toBe(2)
-    expect(robot.y).toBe(1)
-    expect(robot.orientation).toBe('E')
-  })
+    const robot = new Robot(1, 1, 'E', worldGrid, commands);
+    robot.moveForward();
+    expect(robot.x).toBe(2);
+    expect(robot.y).toBe(1);
+    expect(robot.orientation).toBe('E');
+  });
 
   test('Robot can rotate left by 90 degrees (L).', () => {
-    const robot = new Robot(1, 1, 'E', worldGrid, commands)
-    robot.rotateLeft()
-    expect(robot.x).toBe(1)
-    expect(robot.y).toBe(1)
-    expect(robot.orientation).toBe('N')
-  })
+    const robot = new Robot(1, 1, 'E', worldGrid, commands);
+    robot.rotateLeft();
+    expect(robot.x).toBe(1);
+    expect(robot.y).toBe(1);
+    expect(robot.orientation).toBe('N');
+  });
 
   test('Robot can rotate right by 90 degrees (R).', () => {
-    const robot = new Robot(1, 1, 'E', worldGrid, commands)
-    robot.rotateRight()
-    expect(robot.x).toBe(1)
-    expect(robot.y).toBe(1)
-    expect(robot.orientation).toBe('S')
-  })
+    const robot = new Robot(1, 1, 'E', worldGrid, commands);
+    robot.rotateRight();
+    expect(robot.x).toBe(1);
+    expect(robot.y).toBe(1);
+    expect(robot.orientation).toBe('S');
+  });
 
   test('If a robot moves off the grid, it is marked as ‘lost’ and its last valid grid position and orientation is recorded.', () => {
-    const robot = new Robot(1, 1, 'E', worldGrid, commands)
-    robot.moveForward()
-    robot.moveForward()
-    robot.moveForward()
-    robot.moveForward()
-    robot.moveForward()
-    expect(robot.x).toBe(5)
-    expect(robot.y).toBe(1)
-    expect(robot.orientation).toBe('E')
-    expect(robot.isLost).toBe(true)
-  })
-})
+    const robot = new Robot(1, 1, 'E', worldGrid, commands);
+    robot.moveForward();
+    robot.moveForward();
+    robot.moveForward();
+    robot.moveForward();
+    robot.moveForward();
+    expect(robot.x).toBe(5);
+    expect(robot.y).toBe(1);
+    expect(robot.orientation).toBe('E');
+    expect(robot.isLost).toBe(true);
+  });
+});
 
 describe('MarsMissionSimulator', () => {
-  const worldGrid = new WorldGrid(4, 8)
-  const commands = 'LFRFF'
+  const worldGrid = new WorldGrid(4, 8);
+  const commands = 'LFRFF';
 
   test('MarsMissionSimulator can take a string of instructions and execute them on a robot.', () => {
-    const missionSimulator = new MarsMissionSimulator()
-    const robot = new Robot(2, 3, 'E', worldGrid, commands)
-    missionSimulator.processRobot(robot)
-    expect(robot.x).toBe(4)
-    expect(robot.y).toBe(4)
-    expect(robot.orientation).toBe('E')
-  })
+    const missionSimulator = new MarsMissionSimulator();
+    const robot = new Robot(2, 3, 'E', worldGrid, commands);
+    missionSimulator.processRobot(robot);
+    expect(robot.x).toBe(4);
+    expect(robot.y).toBe(4);
+    expect(robot.orientation).toBe('E');
+  });
 
   test('MarsMissionSimulator can generate a robots status report.', () => {
-    const missionSimulator = new MarsMissionSimulator()
-    const robot = new Robot(1, 1, 'E', worldGrid, commands)
-    missionSimulator.generateRobotStatusReport(robot)
-    expect(missionSimulator.statusReports[0]).toBe('(1, 1, E)')
-  })
+    const missionSimulator = new MarsMissionSimulator();
+    const robot = new Robot(1, 1, 'E', worldGrid, commands);
+    missionSimulator.generateRobotStatusReport(robot);
+    expect(missionSimulator.statusReports[0]).toBe('(1, 1, E)');
+  });
 
   test('MarsMissionSimulator can generate a robots status report if it is lost.', () => {
-    const missionSimulator = new MarsMissionSimulator()
-    const robot = new Robot(0, 0, 'E', worldGrid, 'RF')
-    missionSimulator.processRobot(robot)
-    missionSimulator.generateRobotStatusReport(robot)
-    expect(missionSimulator.statusReports[0]).toBe('(0, 0, S) LOST')
-  })
+    const missionSimulator = new MarsMissionSimulator();
+    const robot = new Robot(0, 0, 'E', worldGrid, 'RF');
+    missionSimulator.processRobot(robot);
+    missionSimulator.generateRobotStatusReport(robot);
+    expect(missionSimulator.statusReports[0]).toBe('(0, 0, S) LOST');
+  });
 
   test('MarsMissionSimulator can  and store a world grid.', () => {
-    const missionSimulator = new MarsMissionSimulator()
-    missionSimulator.createWorldGrid(4, 8)
-    expect(missionSimulator.worldGrid).toBeInstanceOf(WorldGrid)
-    expect(missionSimulator.worldGrid?.m).toBe(4)
-    expect(missionSimulator.worldGrid?.n).toBe(8)
-  })
+    const missionSimulator = new MarsMissionSimulator();
+    missionSimulator.createWorldGrid(4, 8);
+    expect(missionSimulator.worldGrid).toBeInstanceOf(WorldGrid);
+    expect(missionSimulator.worldGrid?.m).toBe(4);
+    expect(missionSimulator.worldGrid?.n).toBe(8);
+  });
 
   test('MarsMissionSimulator can create and store robot.', () => {
-    const missionSimulator = new MarsMissionSimulator()
-    missionSimulator.createWorldGrid(4, 8)
-    missionSimulator.createRobot(1, 1, 'E', commands)
-    expect(missionSimulator.robots[0]).toBeInstanceOf(Robot)
-    expect(missionSimulator.robots[0].x).toBe(1)
-    expect(missionSimulator.robots[0].y).toBe(1)
-    expect(missionSimulator.robots[0].orientation).toBe('E')
-    expect(missionSimulator.robots[0].commands).toBe(commands)
-  })
+    const missionSimulator = new MarsMissionSimulator();
+    missionSimulator.createWorldGrid(4, 8);
+    missionSimulator.createRobot(1, 1, 'E', commands);
+    expect(missionSimulator.robots[0]).toBeInstanceOf(Robot);
+    expect(missionSimulator.robots[0].x).toBe(1);
+    expect(missionSimulator.robots[0].y).toBe(1);
+    expect(missionSimulator.robots[0].orientation).toBe('E');
+    expect(missionSimulator.robots[0].commands).toBe(commands);
+  });
 
   test('MarsMissionSimulator can run a simulation with a single robot.', () => {
-    const missionSimulator = new MarsMissionSimulator()
-    missionSimulator.createWorldGrid(4, 8)
-    missionSimulator.createRobot(2, 3, 'E', commands)
-    missionSimulator.runSimulation()
-    expect(missionSimulator.robots[0].x).toBe(4)
-    expect(missionSimulator.robots[0].y).toBe(4)
-    expect(missionSimulator.robots[0].orientation).toBe('E')
-  })
+    const missionSimulator = new MarsMissionSimulator();
+    missionSimulator.createWorldGrid(4, 8);
+    missionSimulator.createRobot(2, 3, 'E', commands);
+    missionSimulator.runSimulation();
+    expect(missionSimulator.robots[0].x).toBe(4);
+    expect(missionSimulator.robots[0].y).toBe(4);
+    expect(missionSimulator.robots[0].orientation).toBe('E');
+  });
 
   test('MarsMissionSimulator can run a simulation with multiple robots.', () => {
-    const missionSimulator = new MarsMissionSimulator()
-    missionSimulator.createWorldGrid(4, 8)
-    missionSimulator.createRobot(2, 3, 'E', commands)
-    missionSimulator.createRobot(0, 2, 'N', 'FFLFRFF')
-    missionSimulator.runSimulation()
-    expect(missionSimulator.robots[0].x).toBe(4)
-    expect(missionSimulator.robots[0].y).toBe(4)
-    expect(missionSimulator.robots[0].orientation).toBe('E')
-    expect(missionSimulator.robots[1].x).toBe(0)
-    expect(missionSimulator.robots[1].y).toBe(4)
-    expect(missionSimulator.robots[1].orientation).toBe('W')
-    expect(missionSimulator.robots[1].isLost).toBe(true)
-    expect(missionSimulator.statusReports).toStrictEqual(['(4, 4, E)', '(0, 4, W) LOST'])
-  })
-})
+    const missionSimulator = new MarsMissionSimulator();
+    missionSimulator.createWorldGrid(4, 8);
+    missionSimulator.createRobot(2, 3, 'E', commands);
+    missionSimulator.createRobot(0, 2, 'N', 'FFLFRFF');
+    missionSimulator.runSimulation();
+    expect(missionSimulator.robots[0].x).toBe(4);
+    expect(missionSimulator.robots[0].y).toBe(4);
+    expect(missionSimulator.robots[0].orientation).toBe('E');
+    expect(missionSimulator.robots[1].x).toBe(0);
+    expect(missionSimulator.robots[1].y).toBe(4);
+    expect(missionSimulator.robots[1].orientation).toBe('W');
+    expect(missionSimulator.robots[1].isLost).toBe(true);
+    expect(missionSimulator.statusReports).toStrictEqual([
+      '(4, 4, E)',
+      '(0, 4, W) LOST',
+    ]);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,197 +1,214 @@
-import readline from 'readline'
+import readline from 'readline';
 
 export class WorldGrid {
-    constructor(public m: number, public n: number) {
-        this.m = m
-        this.n = n
-    }
+  constructor(public m: number, public n: number) {
+    this.m = m;
+    this.n = n;
+  }
 }
 
 export class Robot {
-    isLost: boolean
-    commands: string
-    constructor(public x: number, public y: number, public orientation: string, public worldGrid: WorldGrid, commands: string) {
-        this.x = x
-        this.y = y
-        this.orientation = orientation
-        this.worldGrid = worldGrid
-        this.commands = commands
-        this.isLost = false
+  isLost: boolean;
+  commands: string;
+  constructor(
+    public x: number,
+    public y: number,
+    public orientation: string,
+    public worldGrid: WorldGrid,
+    commands: string,
+  ) {
+    this.x = x;
+    this.y = y;
+    this.orientation = orientation;
+    this.worldGrid = worldGrid;
+    this.commands = commands;
+    this.isLost = false;
+  }
+
+  moveForward() {
+    if (this.isLost) {
+      return;
     }
 
-    moveForward() { 
-        if (this.isLost) {
-            return
+    switch (this.orientation) {
+      case 'N':
+        if (this.y + 1 > this.worldGrid.n) {
+          this.isLost = true;
+          return;
         }
+        this.y++;
+        break;
+      case 'E':
+        if (this.x + 1 > this.worldGrid.m) {
+          this.isLost = true;
+          return;
+        }
+        this.x++;
+        break;
+      case 'S':
+        if (this.y - 1 < 0) {
+          this.isLost = true;
+          return;
+        }
+        this.y--;
+        break;
+      case 'W':
+        if (this.x - 1 < 0) {
+          this.isLost = true;
+          return;
+        }
+        this.x--;
+        break;
+      default:
+        break;
+    }
+  }
 
-        switch (this.orientation) {
-            case 'N':
-                if (this.y + 1 > this.worldGrid.n) {
-                    this.isLost = true
-                    return
-                }
-                this.y++
-                break
-            case 'E':
-                if (this.x + 1 > this.worldGrid.m) {
-                    this.isLost = true
-                    return
-                }
-                this.x++
-                break
-            case 'S':
-                if (this.y - 1 < 0) {
-                    this.isLost = true
-                    return
-                }
-                this.y--
-                break
-            case 'W':
-                if (this.x - 1 < 0) {
-                    this.isLost = true
-                    return
-                }
-                this.x--
-                break
-            default:
-                break
-        }
+  rotateLeft() {
+    if (this.isLost) {
+      return;
     }
 
-    rotateLeft() {
-        if (this.isLost) {
-            return
-        }
+    switch (this.orientation) {
+      case 'N':
+        this.orientation = 'W';
+        break;
+      case 'E':
+        this.orientation = 'N';
+        break;
+      case 'S':
+        this.orientation = 'E';
+        break;
+      case 'W':
+        this.orientation = 'S';
+        break;
+      default:
+        break;
+    }
+  }
 
-        switch (this.orientation) {
-            case 'N':
-                this.orientation = 'W'
-                break
-            case 'E':
-                this.orientation = 'N'
-                break
-            case 'S':
-                this.orientation = 'E'
-                break
-            case 'W':
-                this.orientation = 'S'
-                break
-            default:
-                break
-        }
+  rotateRight() {
+    if (this.isLost) {
+      return;
     }
 
-    rotateRight() {
-        if (this.isLost) {
-            return
-        }
-
-        switch (this.orientation) {
-            case 'N':
-                this.orientation = 'E'
-                break
-            case 'E':
-                this.orientation = 'S'
-                break
-            case 'S':
-                this.orientation = 'W'
-                break
-            case 'W':
-                this.orientation = 'N'
-                break
-            default:
-                break
-        }
+    switch (this.orientation) {
+      case 'N':
+        this.orientation = 'E';
+        break;
+      case 'E':
+        this.orientation = 'S';
+        break;
+      case 'S':
+        this.orientation = 'W';
+        break;
+      case 'W':
+        this.orientation = 'N';
+        break;
+      default:
+        break;
     }
+  }
 }
 
 export class MarsMissionSimulator {
-    robots: Robot[]
-    worldGrid?: WorldGrid
-    statusReports: string[] = []
+  robots: Robot[];
+  worldGrid?: WorldGrid;
+  statusReports: string[] = [];
 
-    constructor() {
-        this.robots = []
+  constructor() {
+    this.robots = [];
+  }
+
+  processRobot(robot: Robot) {
+    for (let i = 0; i < robot.commands.length; i++) {
+      const command = robot.commands[i];
+      switch (command) {
+        case 'F':
+          robot.moveForward();
+          break;
+        case 'L':
+          robot.rotateLeft();
+          break;
+        case 'R':
+          robot.rotateRight();
+          break;
+        default:
+          break;
+      }
     }
+  }
 
-    processRobot(robot: Robot) {
-        for (let i = 0; i < robot.commands.length; i++) {
-            const command = robot.commands[i]
-            switch (command) {
-                case 'F':
-                    robot.moveForward()
-                    break
-                case 'L':
-                    robot.rotateLeft()
-                    break
-                case 'R':
-                    robot.rotateRight()
-                    break
-                default:
-                    break
-            }
-        }
+  generateRobotStatusReport(robot: Robot) {
+    const state = robot.isLost ? ' LOST' : '';
+    this.statusReports.push(
+      `(${robot.x}, ${robot.y}, ${robot.orientation})${state}`,
+    );
+  }
+
+  createWorldGrid(x: number, y: number) {
+    this.worldGrid = new WorldGrid(x, y);
+  }
+
+  createRobot(x: number, y: number, orientation: string, commands: string) {
+    if (this.worldGrid) {
+      const robot = new Robot(x, y, orientation, this.worldGrid, commands);
+      this.robots.push(robot);
     }
+  }
 
-    generateRobotStatusReport(robot: Robot) {
-        const state = robot.isLost ? ' LOST' : ''
-        this.statusReports.push(`(${robot.x}, ${robot.y}, ${robot.orientation})${state}`)
-    }
+  runSimulation() {
+    this.robots.forEach((robot) => {
+      this.processRobot(robot);
+      this.generateRobotStatusReport(robot);
+    });
 
-    createWorldGrid(x: number, y: number) {
-        this.worldGrid = new WorldGrid(x, y)
-    }
-
-    createRobot(x: number, y: number, orientation: string, commands: string) {
-        if (this.worldGrid) {
-            const robot = new Robot(x, y, orientation, this.worldGrid, commands)
-            this.robots.push(robot)
-        }
-    }
-
-    runSimulation() {
-        this.robots.forEach((robot) => {
-            this.processRobot(robot)
-            this.generateRobotStatusReport(robot)
-        })
-
-        this.statusReports.forEach((statusReport) => {
-            console.log(statusReport)
-        })
-    }
+    this.statusReports.forEach((statusReport) => {
+      // eslint-disable-next-line no-console
+      console.log(statusReport);
+    });
+  }
 }
 
-const marsMissionSimulator = new MarsMissionSimulator()
+const marsMissionSimulator = new MarsMissionSimulator();
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-})
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
 
-let lineCount = 0
-let worldGridDimensions: string[] = []
-let robotInput: string[] = []
-let robotString: string = ''
+let worldGridDimensions: string[] = [];
+let robotInput: string[] = [];
+let robotString = '';
 
-rl.question('Enter the world dimensions then enter robot inputs: ', (response) => {
-    worldGridDimensions = response.split(' ')
-    marsMissionSimulator.createWorldGrid(parseInt(worldGridDimensions[0]), parseInt(worldGridDimensions[1]))
+rl.question(
+  'Enter the world dimensions then enter robot inputs: ',
+  (response) => {
+    worldGridDimensions = response.split(' ');
+    marsMissionSimulator.createWorldGrid(
+      parseInt(worldGridDimensions[0]),
+      parseInt(worldGridDimensions[1]),
+    );
 
     rl.on('line', (line) => {
-        if (line.toLowerCase() === 'end' || line === '') {
-            rl.close()
-            return
-        }
+      if (line.toLowerCase() === 'end' || line === '') {
+        rl.close();
+        return;
+      }
 
-        robotString = line.replace('(', '').replace(')', ',')
-        robotInput = robotString.split(',')
-        marsMissionSimulator.createRobot(parseInt(robotInput[0]), parseInt(robotInput[1]), robotInput[2].trim(), robotInput[3].trim())
-    
-        lineCount++
-    }) 
-})
+      robotString = line.replace('(', '').replace(')', ',');
+      robotInput = robotString.split(',');
+      marsMissionSimulator.createRobot(
+        parseInt(robotInput[0]),
+        parseInt(robotInput[1]),
+        robotInput[2].trim(),
+        robotInput[3].trim(),
+      );
+    });
+  },
+);
 
 rl.on('close', () => {
-    marsMissionSimulator.runSimulation()
-})
+  marsMissionSimulator.runSimulation();
+});
